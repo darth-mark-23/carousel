c681e00e62e0cf7bdbd60528935c6c68091eab22
Darth Mark <darth.mark.23@gmail.com>
2023-07-04 19:53:05 -0700

    test(Carousel-8): Ensure tests are up to date

--- src/test_get_completion.py
+++ src/test_get_completion.py
@@ -1,27 +1,45 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
+import os
 from get_completion import get_completion
 
 class TestGetCompletion(unittest.TestCase):
-    @patch('openai.ChatCompletion.create')
-    def test_get_completion(self, mock_create):
+    def setUp(self):
+        self.model = 'gpt-3.5-turbo-0613'
+        self.functions = [{'name': 'function1'}, {'name': 'function2'}]
+        self.messages = [{'role': 'system', 'content': 'Hello, how can I help you?'}]
 
-        mock_choice = mock_create.return_value.choices[0]
-        mock_choice.message.content = "Mock response"
-        
-        prompt = "Test prompt"
-        result = get_completion(prompt)
-        
+    def test_get_completion_with_none_model(self):
+        with self.assertRaises(Exception) as context:
+            get_completion(None, self.functions, self.messages)
+        self.assertTrue('Completion requested with missing model parameter' in str(context.exception))
+
+    def test_get_completion_with_none_functions(self):
+        with self.assertRaises(Exception) as context:
+            get_completion(self.model, None, self.messages)
+        self.assertTrue('Completion requested with missing functions parameter' in str(context.exception))
+
+    def test_get_completion_with_none_messages(self):
+        with self.assertRaises(Exception) as context:
+            get_completion(self.model, self.functions, None)
+        self.assertTrue('Completion requested with blank messages parameter' in str(context.exception))
+
+    @patch('openai.ChatCompletion.create')
+    def test_get_completion_calls_openai_create(self, mock_create):
+        # Arrange
+        mock_create.return_value = MagicMock()
+
+        # Act
+        result = get_completion(self.model, self.functions, self.messages)
+
+        # Assert
         mock_create.assert_called_once_with(
-            model='gpt-3.5-turbo',
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant who speaks in cockney slang."},
-                {"role": "user", "content": prompt}
-            ],
+            model=self.model,
+            messages=self.messages,
+            functions=self.functions,
             max_tokens=2000,
             temperature=0.7
         )
-        self.assertEqual(result, "Mock response")
 
-# if __name__ == '__main__':
-#     unittest.main()
+if __name__ == '__main__':
+    unittest.main()
